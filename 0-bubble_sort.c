07aef55bb0aaa40e8aa33bc61e8621bf93ecc4a2
Sphax <itssphax123@gmail.com>
2023-12-14 11:30:16 -0800

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort an array
+ * @array: array to sort
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, tmp;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i;j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
