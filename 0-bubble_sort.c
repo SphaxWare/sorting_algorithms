6b24353238c860d1a41776eaab98c6ca8702e24a
Sphax <itssphax123@gmail.com>
2023-12-15 08:25:44 -0800

    solution

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,7 +13,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1 - i;j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
