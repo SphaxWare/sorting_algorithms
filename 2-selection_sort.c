ea0e49c7178a746888ce644cf2d65b73d22fb66f
Sphax <itssphax123@gmail.com>
2023-12-16 14:57:08 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,28 +1,33 @@
 #include "sort.h"
-/*
- * selection_sort - sort an array
- * @array:array
+/**
+ * selection_sort - sort array
+ * @array: array
  * @size: size of array
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int min;
+	int min, index, tmp;
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = array[i];
-		for (j = i + 1;j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < min)
 			{
 				min = array[j];
-				array[j] = array[i];
-				array[i] = min;
-				print_array(array, size);
+				index = j;
 			}
+		}
+		if (min < array[i])
+		{
+			tmp = array[i];
+			array[i] = min;
+			array[index] = tmp;
+			print_array(array, size);
 		}
 	}
 }
