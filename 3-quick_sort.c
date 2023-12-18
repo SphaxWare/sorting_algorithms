64711a996e1fa2b3e585aa9fcd94d419e378cce1
Sphax <itssphax123@gmail.com>
2023-12-18 09:18:48 -0800

    test quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,67 @@
+#include "sort.h"
+/**
+ * quick_sort - sort array
+ * @array:array
+ * @size:size
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	quicksort_recursive(array, 0, size - 1, size);
+}
+/**
+ * swap - swaps two integers
+ * @x: first integer
+ * @y: second integer
+ */
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+/**
+ * partition - partitions the array and returns the pivot index
+ * @array: array to be partitioned
+ * @low: starting index
+ * @high: ending index
+ * @size: size of the array
+ * Return: pivot index
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int j, i = low;
+
+    for (j = low; j < high; j++)
+    {
+        if (array[j] <=  pivot)
+        {
+            swap(&array[i], &array[j]);
+            print_array(array, size);
+	    i++;
+        }
+    }
+    swap(&array[i], &array[high]);
+    print_array(array, size);
+    return (i);
+}
+
+/**
+ * quicksort_recursive - recursive function to implement quicksort
+ * @array: array to be sorted
+ * @low: starting index
+ * @high: ending index
+ * @size: size of the array
+ */
+void quicksort_recursive(int *array, int low, int high, size_t size)
+{
+    if (low < high)
+    {
+        int pi = partition(array, low, high, size);
+
+        quicksort_recursive(array, low, pi - 1, size);
+        quicksort_recursive(array, pi + 1, high, size);
+    }
+}
--- sort.h
+++ sort.h
@@ -25,4 +25,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swaper(listint_t *previous, listint_t *current);
+void swap(int *a, int *b);
+int partition(int *array, int low, int high, size_t size);
+void quicksort_recursive(int *array, int low, int high, size_t size);
 #endif
